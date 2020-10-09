71a8d7af4e398e6bbaa5f3eac204b482b3a2bc47
Souvik <souvik.9401@gmail.com>
2020-10-09 21:48:49 +0530

    code for express-rate-limit module included

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ var http = require('http');
 var path = require("path");
 var bodyParser = require('body-parser');
 var helmet = require('helmet');
+var rateLimit = require("express-rate-limit");
+
+
 
 var app = express();
 var server = http.createServer(app);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
 
 
 var db = new sqlite3.Database('./database/employees.db');
@@ -15,6 +23,7 @@ var db = new sqlite3.Database('./database/employees.db');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'./public')));
 app.use(helmet());
+app.use(limiter);
 
 db.run('CREATE TABLE IF NOT EXISTS emp(id TEXT, name TEXT)');
 
